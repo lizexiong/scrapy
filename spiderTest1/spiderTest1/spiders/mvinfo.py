3672da11292d22c1ffa5c406c8d3ab03e944c5a3
lizexiong <1043460476@qq.com>
2018-06-18 16:37:41 +0800

    1.8 scrapy item 使用与提取信息保存

--- spiderTest1/spiderTest1/spiders/mvinfo.py
+++ spiderTest1/spiderTest1/spiders/mvinfo.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import scrapy
 from scrapy import Request			#发起下一次请求
+from ..items import Spidertest1Item		#导入items
 
 class MvinfoSpider(scrapy.Spider):	
     name = 'mvinfo'
@@ -13,11 +14,15 @@ class MvinfoSpider(scrapy.Spider):
         movie_list = response.xpath("//div[@class='hd']/a")
         baseurl = 'https://movie.douban.com/top250'
         for i in movie_list:
-        	print (i.xpath("span[1]/text()").extract(),i.xpath("@href").extract())
-        	self.fsave.writelines(i.xpath('span[1]/text()').extract(),)	#writelines()   参数必须是list/string/dict/tuple 等可迭代序列对象，且里面内容须是字符，
-        	self.fsave.write(" ")
-        	self.fsave.writelines(i.xpath('@href').extract())
-        	self.fsave.write("\n")
+        	# print (i.xpath("span[1]/text()").extract(),i.xpath("@href").extract())
+        	# self.fsave.writelines(i.xpath('span[1]/text()').extract(),)	#writelines()   参数必须是list/string/dict/tuple 等可迭代序列对象，且里面内容须是字符，
+        	# self.fsave.write(" ")
+        	# self.fsave.writelines(i.xpath('@href').extract())
+        	# self.fsave.write("\n")
+        	item = Spidertest1Item()
+        	name = i.xpath("span[1]/text()").extract()
+        	item['name'] = name
+        	yield item
 
         nextpage = response.xpath("//span[@class='next']/a/@href").extract()
         if nextpage:
