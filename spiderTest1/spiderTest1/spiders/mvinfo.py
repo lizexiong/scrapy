d762f29d78d025cbffc3530332a0e34e7c6ab624
lizexiong <1043460476@qq.com>
2018-06-18 14:08:29 +0800

    1.5 提取页面信息

--- spiderTest1/spiderTest1/spiders/mvinfo.py
+++ spiderTest1/spiderTest1/spiders/mvinfo.py
@@ -1,13 +1,21 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from scrapy import Request			#发起下一次请求
 
-
-class MvinfoSpider(scrapy.Spider):
+class MvinfoSpider(scrapy.Spider):	
     name = 'mvinfo'
     #allowed_domains = ['example.com']
     start_urls = ['https://movie.douban.com/top250']
 
     def parse(self, response):
         print ("*" * 50 )
-        print (response.url)
-        print (response.body)
+        movie_list = response.xpath("//div[@class='hd']/a")
+        baseurl = 'https://movie.douban.com/top250'
+        for i in movie_list:
+        	print (i.xpath("span[1]/text()").extract(),i.xpath("@href").extract())
+
+        nextpage = response.xpath("//span[@class='next']/a/@href").extract()
+        if nextpage:
+        	nexturl = baseurl + nextpage[0]
+        	print (nexturl)
+        	yield Request(nexturl, callback=self.parse)		#yield解析下一页
