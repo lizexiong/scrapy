57e4aebb6cd65420c026b6aaea05ea1be17e7da6
lizexiong <1043460476@qq.com>
2018-06-18 20:00:51 +0800

    2.1scrapy 实战二

--- spider_douban/spider_douban/spiders/douban.py
+++ spider_douban/spider_douban/spiders/douban.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import scrapy
 from scrapy import Request
+import re
 
 class DoubanSpider(scrapy.Spider):
     name = 'douban'
@@ -11,6 +12,23 @@ class DoubanSpider(scrapy.Spider):
     	detail_list = response.xpath("//div[@class='hd']/a/@href").extract()	#提取电影详细页面信息
     	for i in detail_list:
     		yield Request(i, callback=self.details_parse)
+    		return 
+
+    def getInfoByRe(self,instr,restr):
+    	m = re.search(restr, instr, re.S) 	#re.S 把/n也当作字符串来匹配
+    	if m:
+    		info = m.groups()[0]
+    	else:
+    		info = ""
+    	return info
 
     def details_parse(self, response):
-    	print (response.url)
+        director = response.xpath("//a[@rel='v:directedBy']/text()").extract()		#导演
+        starring = response.xpath("//a[@rel='v:starring']/text()").extract()		#主演
+        mvtype = response.xpath("//span[@rel='v:genre']/text()").extract()			#电影类型
+        showplace = self.getInfoByRe(response.text, r'制片国家/地区:</span>(.+?)<br/>')		#制作国家
+        lan = self.getInfoByRe(response.text, r'语言:</span>(.+?)<br/>')			#语言
+        mvnames = self.getInfoByRe(response.text, r'又名:</span>(.+?)<br/>')		#电影别名
+        mvshowtime = response.xpath("//span[@property='v:initialReleaseDate']/text()").extract()		#电影上映时间
+        mvtimelen = response.xpath("//span[@property='v:runtime']/text()").extract()			#电影时长
+        print (director,starring,mvtype,showplace,lan,mvnames,mvshowtime,mvtimelen)
