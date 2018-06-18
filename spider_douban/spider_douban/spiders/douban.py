cc5cb76ca2e482a60b45ef5ddeeb729519dab989
lizexiong <1043460476@qq.com>
2018-06-19 00:14:22 +0800

    2.6scrapy 实战五 未完成版本

--- spider_douban/spider_douban/spiders/douban.py
+++ spider_douban/spider_douban/spiders/douban.py
@@ -13,9 +13,17 @@ class DoubanSpider(scrapy.Spider):
     start_urls = ['https://movie.douban.com/top250']
 
     def parse(self, response):
-    	detail_list = response.xpath("//div[@class='hd']/a/@href").extract()	#提取电影详细页面信息
+    	detail_list = response.xpath("//div[@class='hd']/a")		#提取电影详细页面信息
+    	'''
+    		这里更改为response.xpath("//div[@class='hd']/a")是因为要获取其它值（如评分等）,不止是url,
+    		所以不能在detail_list的时候就直接获取到url的值,否则就获取一个固定死的url值,别的值就不方便获取
+    		了
+    	'''
     	for i in detail_list:
-    		yield Request(i, callback=self.details_parse)
+    		print (i)
+    		href = i.xpath('@href').extract()[0]
+    		print (href,'+++' * 30)
+    		yield Request(href, callback=self.details_parse)
     		return 
 
     def getInfoByRe(self,instr,restr):
